a59f5b4ac026bf7bce1bb744c0b8e99c65d334d4
KONE Fousseni <fkone@gmail.com>
2025-01-09 17:34:30 +0000

    version 2

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet">
     <script src="//code.iconify.design/1/1.0.6/iconify.min.js"></script>
     @vite('resources/js/app.js')
