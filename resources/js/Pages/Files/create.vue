8099df6a989d55fe6aea53329fa456c21d276517
KONE Fousseni <fkone@gmail.com>
2025-01-09 12:29:22 +0000

    version 2

--- resources/js/Pages/Files/create.vue
+++ resources/js/Pages/Files/create.vue
@@ -5,7 +5,31 @@
         <h1 class="text-blue text-2xl sm:text-3xl">Ajout d'un Fichier</h1>
       </div>
       <form @submit.prevent="submit" enctype="multipart/form-data">
-        
+        <div class="space-y-6">
+          <InputComponent
+            name="file_link"
+            type="file"
+            :errors="$page.props.errors.file_link"
+            placeholder="Sélectionner le fichier"
+            @input="fileChange"
+            divClass="w-full border"
+            inputClass="w-full"
+          />
+          <SelectComponent
+            name="folders"
+            :options="folders"
+            :errors="$page.props.errors.folders"
+            v-model="form.folder_id"
+            placeholder="Sélectionner un Dossier"
+            divClass="w-full"
+          />
+          <div class="flex justify-center">
+            <ButtonComponent
+              content="Créer le fichier"
+              customClass="bg-blue text-white px-6 py-2"
+            />
+          </div>
+        </div>
       </form>
     </div>
   </Dashboard>
@@ -13,8 +37,10 @@
 
 <script setup>
 import { useForm } from '@inertiajs/vue3';
-
-import Dashboard from './../dashboard/dashboard.vue';
+import InputComponent from '../components/InputComponent.vue';
+import SelectComponent from '../components/SelectComponent.vue';
+import ButtonComponent from '../components/ButtonComponent.vue';
+import Dashboard from '../dashboard/dashboard.vue';
 
 const props = defineProps({
   folders: Object,
--- resources/js/Pages/Files/edit.vue
+++ resources/js/Pages/Files/edit.vue
@@ -5,17 +5,41 @@
         <h1 class="text-blue text-3xl">Modifier le Fichier</h1>
       </div>
       <form @submit.prevent="submit">
-          
+        <div class="space-y-4">
+          <InputComponent
+            name="file_link"
+            type="file"
+            placeholder="selectionner le fichier"
+            :errors="$page.props.errors.file_link"
+            @change="fileChange"
+            divClass="mb-4 w-full border"
+            inputClass="w-full"
+          />
+          <SelectComponent
+            name="folders"
+            :options="folders"
+            v-model="form.folder_id"
+            :errors="$page.props.errors.folders"
+            placeholder="Sélectionner un Dossier"
+            divClass="mb-4"
+          />
+          <ButtonComponent
+            content="Modifier le Fichier"
+            customClass="bg-blue text-white px-4"
+          />
+        </div>
       </form>
     </div>
   </Dashboard>
 </template>
 
 <script setup>
+import InputComponent from '../Components/InputComponent.vue';
 import { ref } from 'vue';
 import { router } from '@inertiajs/vue3';
-
-import Dashboard from './../dashboard/dashboard.vue';
+import SelectComponent from '../Components/SelectComponent.vue';
+import ButtonComponent from '../Components/ButtonComponent.vue';
+import Dashboard from '../dashboard/dashboard.vue';
 
 const props = defineProps(["file", "folders"]) 
 
