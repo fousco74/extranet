e7ab8a9e9b80a19239af0fed14b113c847c1cdd1
KONE Fousseni <fkone@gmail.com>
2025-01-09 12:05:46 +0000

    version 2

--- resources/js/Pages/Files/edit.vue
+++ resources/js/Pages/Files/edit.vue
@@ -5,29 +5,7 @@
         <h1 class="text-blue text-3xl">Modifier le Fichier</h1>
       </div>
       <form @submit.prevent="submit">
-        <div class="space-y-4">
-          <InputComponent
-            name="file_link"
-            type="file"
-            placeholder="selectionner le fichier"
-            :errors="$page.props.errors.file_link"
-            @change="fileChange"
-            divClass="mb-4 w-full border"
-            inputClass="w-full"
-          />
-          <SelectComponent
-            name="folders"
-            :options="folders"
-            v-model="form.folder_id"
-            :errors="$page.props.errors.folders"
-            placeholder="Sélectionner un Dossier"
-            divClass="mb-4"
-          />
-          <ButtonComponent
-            content="Modifier le Fichier"
-            customClass="bg-blue text-white px-4"
-          />
-        </div>
+          
       </form>
     </div>
   </Dashboard>
